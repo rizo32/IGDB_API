500fe07cd83f6131aa182d0d8523dd4d60baf38e
Rizo <g.rizzoli@hotmail.ca>
2023-02-18 16:13:46 -0500

    gestion api, debut routage

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require("express");
-const path = require("path");
+const path = require('path');
 const app = express();
 const { PORT } = require("./config");
-const { router } = require("./routes");
+const { update_stock_titles } = require("./web_scraping");
+const { fetchCandles } = require("./controllers/finnController");
+
 
 // Utilisation du middleware pour parser les requêtes en JSON
 app.use(express.json());
 
-app.use(router);
+// Route pour récupérer les candles
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
-app.listen(80 || process.env.port || PORT || 4001, () => {
+app.get("/*", function(req, res){
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+})
+
+// app.get('/candles=:symbol', fetchCandles);
+// app.get('/candles', );
+
+
+// Update de la liste des stock à chaque jour
+setInterval(update_stock_titles, 24 * 60 * 60 * 1000);
+// Commande pour update manuel:
+// update_stock_titles();
+
+
+
+app.listen(process.env.port || PORT || 8080, () => {
   console.log(`Server started on port ${PORT}`);
 });
--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
-    CLIENT_ID: process.env.client_id,
-    CLIENT_SECRET: process.env.client_secret
+    API_KEY: process.env.API_KEY,
+    WEB_HOOK: process.env.WEB_HOOK
 }
